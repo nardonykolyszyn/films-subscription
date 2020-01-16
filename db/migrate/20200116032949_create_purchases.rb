b0f5b38819d90e33de0928fd387895d06d3232e7
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2020-01-15 22:44:52 -0500

    add expires_at on purchases

--- db/migrate/20200116032949_create_purchases.rb
+++ db/migrate/20200116032949_create_purchases.rb
@@ -5,7 +5,7 @@ class CreatePurchases < ActiveRecord::Migration[6.0]
     create_table :purchases do |t|
       t.belongs_to :user, null: false, foreign_key: { on_delete: :cascade }, index: true
       t.belongs_to :purchase_option, null: false, foreign_key: { on_delete: :cascade }, index: true
-
+      t.date :expires_at
       t.timestamps
     end
   end
