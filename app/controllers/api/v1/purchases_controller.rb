5f53a142158d66ce97fd6c433b4e8eb8f1986126
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2020-01-16 00:37:16 -0500

    cut down logic

--- app/controllers/api/v1/purchases_controller.rb
+++ app/controllers/api/v1/purchases_controller.rb
@@ -2,12 +2,32 @@
 
 module Api
   module V1
-    class SeasonsController < SitesController
-      def index
-        seasons = Films::Season.all
-        render json: {
-          seasons: SeasonSerializer.new(sea)
-        }, status: :ok
+    class PurchasesController < SitesController
+      def create
+        purchase = Purchase.new(purchase_params)
+        purchase.expires_at = Time.now + 3.days
+        @data = purchase.save ? handle_valid_for(purchase) : handle_invalid_for(purchase)
+
+        render json: @data, status: purchase.persisted? ? :ok : :unprocessable_entity
+      end
+
+      private
+
+      def handle_valid_for(resource)
+        {
+          purchase: resource,
+          message: 'Your purchase has been created'
+        }
+      end
+
+      def handle_invalid_for(resource)
+        {
+          errors: resource.errors.full_messages.to_sentence
+        }
+      end
+
+      def purchase_params
+        params.require(:purchase).permit(:user_id, :purchase_option_id)
       end
     end
   end
