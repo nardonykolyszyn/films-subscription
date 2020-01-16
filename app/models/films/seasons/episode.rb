3ed09a0408e14d1877d5bcebe6d4e4fe0acefb04
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2020-01-15 21:17:15 -0500

    validate sequence_number

--- app/models/films/seasons/episode.rb
+++ app/models/films/seasons/episode.rb
@@ -2,6 +2,9 @@
 
 module Films::Seasons
   class Episode < ApplicationRecord
+    ## Associations
     belongs_to :season
+    ## Validations
+    validates_numericality_of :sequence_number, greater_than: 0
   end
 end
