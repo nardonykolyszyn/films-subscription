63a2bb8fccf9cb979b45b4c94431dae4a54eba51
devpolish <nardonykolyszyn@gmail.com>
2020-01-16 12:04:38 -0500

    remove useless controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :movies, only: :index
-      resources :seasons, only: :index
       resources :films, only: :index
       resources :purchases, only: :create
       resource  :library, only: :show
