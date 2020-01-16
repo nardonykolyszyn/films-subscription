0308ea7d63a4e5a23155ee47527c96b555078f59
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2020-01-16 10:25:05 -0500

    handle not_found user

--- app/controllers/api/v1/libraries_controller.rb
+++ app/controllers/api/v1/libraries_controller.rb
@@ -4,11 +4,21 @@ module Api
   module V1
     class LibrariesController < SitesController
       def show
-        user = User.find(params[:user_id])
+        user = User.find_by(id: params[:user_id])
+        return not_found if user.nil?
         render json: {
           library: user.current_library
         }, status: :ok
       end
+
+      private
+
+      # Override behavior of knock to insert a custom message when the user is not authenticated
+      # source: https://github.com/nsarno/knock/blob/master/app/controllers/knock/application_controller.rb
+      def not_found
+        render json: { message: 'This user could not be retrieved' }, status: :not_found
+      end
+
     end
   end
 end
