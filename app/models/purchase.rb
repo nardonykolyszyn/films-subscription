54fd042413b11f5df366dc3e1672159507befae6
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2020-01-16 00:28:13 -0500

    validate uniqueness of purchase_option scope

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -6,6 +6,11 @@ class Purchase < ApplicationRecord
   belongs_to :purchase_option
   has_one    :product, through: :purchase_option
   ## Validations
-  # NOTE: Validate purchase_option uniqueness.
-  # validate :unique_purchase_option
+  validate :unique_purchase_option
+
+  def unique_purchase_option
+    existing_purchase = user.purchases
+                            .where(purchase_option_id: purchase_option.id).any?
+    errors.add(:purchase_option, 'You have already bought this film.') if existing_purchase
+  end
 end
