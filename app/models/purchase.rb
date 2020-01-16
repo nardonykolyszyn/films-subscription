3aea590cf9764c415b7b382256e39c0472ca6363
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2020-01-15 22:46:18 -0500

    add product on purchases

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -4,4 +4,5 @@ class Purchase < ApplicationRecord
   ## Associations
   belongs_to :user
   belongs_to :purchase_option
+  has_one    :product, through: :purchase_option
 end
--- app/models/purchase_option.rb
+++ app/models/purchase_option.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class PurchaseOption < ApplicationRecord
   belongs_to :product
 end
